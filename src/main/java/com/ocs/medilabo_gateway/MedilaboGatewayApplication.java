3f3507ba9522345bbbe4250d0a187cf8498da502
echonoprog <a521rr@login.axa>
2024-04-26 17:42:02 +0200

    Link with patients

---
+++ src/main/java/com/ocs/medilabo_gateway/MedilaboGatewayApplication.java
@@ -0,0 +1,14 @@
+package com.ocs.medilabo_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class MedilaboGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedilaboGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ocs/medilabo_gateway/MedilaboGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ocs.medilabo_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedilaboGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
